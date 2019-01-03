fd161f38006bce92c531b4c30a87f85b05e13b94
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 17:35:54 +0700

    Add files via upload

---
+++ menyalin Array.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main (void)
+{
+    int c[5] = {1,2,3,4,5};
+    int d[5];
+    int i=0, j=0
+    printf("Nilai array yang akan di tampilkan\n");
+    for (i=4; i>=0; i--){
+        d[i]=c[j];
+        printf("%d", c[j]);
+        j++;
+    }
+    printf ("\n");
+    printf ("Hasil salisan dari array A adalah sebagai berikut\n");
+    for (i=4; i>=0; i--){
+        printf("%d", d[i]);
+    }
+    printf ("\n");
+    return 0;
+}
